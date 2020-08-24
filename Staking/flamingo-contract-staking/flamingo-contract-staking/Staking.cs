69add67c4d67485a3553122c46a32923d79485ab
晨 黎 <lichen@neo.org>
2020-08-24 15:07:17 +0800

    add dependency

---
+++ Staking/flamingo-contract-staking/flamingo-contract-staking/Staking.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services;
+
+namespace flamingo_contract_staking
+{
+    public partial class Staking : SmartContract
+    {
+    }
+}
